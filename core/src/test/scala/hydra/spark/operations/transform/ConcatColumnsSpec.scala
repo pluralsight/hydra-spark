3ae1978c388eb10a0332c5da91f08a9222d8a38c
dvannoy <dustinvannoy@gmail.com>
2017-01-23 11:28:17 -0800

    Add ability to parse List in dsl so that ConcatColumns can be called through dsl

--- core/src/test/scala/hydra/spark/operations/transform/ConcatColumnsSpec.scala
+++ core/src/test/scala/hydra/spark/operations/transform/ConcatColumnsSpec.scala
@@ -1,7 +1,8 @@
 package hydra.spark.operations.transform
 
+import hydra.spark.dsl.parser.TypesafeDSLParser
 import hydra.spark.testutils.{SharedSparkContext, StaticJsonSource}
-import org.apache.spark.sql.{SQLContext, DataFrame}
+import org.apache.spark.sql.{SQLContext}
 import org.scalatest.{FunSpecLike, Matchers}
 
 /**
@@ -10,10 +11,40 @@ import org.scalatest.{FunSpecLike, Matchers}
 class ConcatColumnsSpec extends Matchers with FunSpecLike with SharedSparkContext {
 
   describe("It should concatenate two columns") {
-    it("Should allow use of dataframe columns in expressions") {
+    it("Should allow use of two dataframe columns to make one column") {
       val sqlContext = new SQLContext(sc)
-      val df = ConcatColumns(Seq("email","msg-no"), "newColumn").transform(StaticJsonSource.createDF(sqlContext))
-      df.first().getString(3) shouldBe "alex-silva@ps.com|0"
+      // ConcatColumns accepts a sequence but dsl will actually send the subtype list
+      val df1 = ConcatColumns(List("email","msg-no"), "newColumn").transform(StaticJsonSource.createDF(sqlContext))
+      df1.first().getString(3) shouldBe "alex-silva@ps.com|0"
     }
+    it("Should be parseable") {
+       val dsl =
+      """
+      |{
+        |    "dispatch": {
+          |        "version": 1,
+          |        "spark.master": "local[*]",
+          |        "name": "test-dispatch",
+          |        "source": {
+          |            "hydra.spark.testutils.ListSource": {
+          |                "messages": ["1", "2", "3"]
+          |            }
+          |        },
+          |        "operations": {
+          |          "concat-columns": {
+          |           "newName": "newCol",
+          |           "columnNames": ["col1", "col2"]
+          |           }
+          |        }
+          |    }
+        |}
+      """.stripMargin
+
+      val dispatch = TypesafeDSLParser().parse(dsl)
+      val d = dispatch.operations.steps.head.asInstanceOf[ConcatColumns]
+      d.columnNames shouldBe Seq("col1","col2")
+      d.newName shouldBe "newCol"
+    }
+
   }
 }
